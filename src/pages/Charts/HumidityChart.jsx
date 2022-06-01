be77cfb0573cc120909cc12084388907be475403
noah <nspielman@live.com>
2022-05-31 22:59:58 -0600

    components and pages

---
+++ src/pages/Charts/HumidityChart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HumidityChart = () => {
+  return (
+    <div>HumidityChart</div>
+  )
+}
+
+export default HumidityChart
---
+++ src/pages/Charts/TemperatureChart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TemperatureChart = () => {
+  return (
+    <div>TemperatureChart</div>
+  )
+}
+
+export default TemperatureChart
