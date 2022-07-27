945686bcff87f7b12370a0dd80ef163464c3d2fb
Noah <nspielman@live.com>
2022-07-27 18:07:04 -0400

    landing page

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { IoIosMore } from 'react-icons/io';
+import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
+
+import { LineChart, SparkLine } from '../components/Charts';
+
+import product9 from '../data/product9.jpg';
+import {
+  earningData,
+  medicalproBranding,
+  recentTransactions,
+  weeklyStats,
+  dropdownData,
+  SparklineAreaData,
+  ecomPieChartData,
+} from '../data/dummy';
+
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Overview = () => {
   return (
-    <div>Overview</div>
-  )
-}
+    <div className='mt-12'>
+      <div className='flex flex-wrap lg:flex-nowrap justify-center'>
+        <div className='bg-white dark:text-gray-300 dark:bg-secondary-dark-bg h-44 
+        rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg'></div>
+      </div>
+    </div>
+  );
+};
 
-export default Overview
+export default Overview;
