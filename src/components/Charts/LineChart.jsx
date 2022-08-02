f23f9e305c3d8521c3d18bddb14c4116999949c3
Noah <nspielman@live.com>
2022-08-02 16:52:48 -0400

    linegraph

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -22,11 +22,11 @@ ChartJS.register(
   Legend
 );
 
-export const options = {
+const options = {
   responsive: true,
   plugins: {
     legend: {
-      position: 'top' as const,
+      position: 'top',
     },
     title: {
       display: true,
@@ -37,24 +37,34 @@ export const options = {
 
 const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      label: 'Dataset 2',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-  ],
-};
+class LineChart extends React.Component {
+  constructor() {
+    super(...arguments)
+    this.data = {
+      labels,
+      datasets: [
+        {
+          label: 'Temp',
+          data: labels.map(() =>
+            faker.datatype.number({ min: -1000, max: 1000 })
+          ),
+          borderColor: 'rgb(255, 99, 132)',
+          backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
+        {
+          label: 'Dataset 2',
+          data: labels.map(() =>
+            faker.datatype.number({ min: -1000, max: 1000 })
+          ),
+          borderColor: 'rgb(53, 162, 235)',
+          backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        },
+      ],
+    };
+  }
 
-export function App() {
-  return <Line options={options} data={data} />;
+  render() {
+    return <Line options={options} data={this.data} />;
+  }
 }
+export default LineChart;
